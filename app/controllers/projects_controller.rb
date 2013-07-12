bf628772cdcc5b9ea48a722af28cf51714910e97
bvuongx <bvuong@gmail.com>
2013-07-11 21:48:32 -0700

    Add the ability to view a list of all projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
 
-	def index
-	end
+def index
+    @projects = Project.all
+  end
 
   def new
     @project = Project.new
@@ -9,18 +10,16 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(params[:project])
-
-  if @project.save
-    flash[:notice] = "Project has been created."
-    redirect_to @project
-  else
-    flash[:alert] = "Project has not been created."
-    render :action => "new"
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been created."
+      render :action => "new"
+    end
   end
-end
 
   def show
-    @project = Project.find(params[:id])
   end
 
 private
---
+++ spec/support/factories/project_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do |variable|
+  factory :project do
+    name "Example project"
+  end
+end
